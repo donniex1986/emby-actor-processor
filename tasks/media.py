e0d2439cee8db507cb1b831b07cf2df77489cc5f
hbq0405 <hbq0405@hotmail.com>
2026-07-26 15:39:10 +0800

    补齐缺失的官方分级

--- tasks/media.py
+++ tasks/media.py
@@ -827,8 +827,9 @@ def task_populate_metadata_cache(
                           OR NULLIF(BTRIM(title), '') IS NULL
                           OR NULLIF(BTRIM(overview), '') IS NULL
                           OR release_year IS NULL
-                          OR NULLIF(BTRIM(poster_path), '') IS NULL
-                          OR NULLIF(BTRIM(backdrop_path), '') IS NULL
+                          OR NULLIF(BTRIM(COALESCE(
+                              official_rating_json->>'US', official_rating_json->>'us', '')),
+                              '') IS NULL
                           OR genres_json IS NULL
                           OR genres_json = '[]'::jsonb
                           OR (
